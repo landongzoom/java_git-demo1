4dc7a24104d9e2638e18da544e452abb15975688
“landong” <lan@zoom.com>
2022-04-05 23:18:00 +0800

    update four：做了一个方法，根据时间毫秒值来判断User接下来要干的事情

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,12 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Test {
+    private static long time = System.currentTimeMillis();
     public static void main(String[] args) {
         User u = new User();
         u.setName("小白");
@@ -12,7 +20,26 @@ public class Test {
         System.out.println("跑了15分钟");
         u.runStop();
 
-        int count = 3;
-        System.out.println(count);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        // 做一个方法
+        if(time % 2 == 1){
+            playComputer(u);
+        }else{
+            playPhone(u);
+        }
+
+
+
+    }
+
+    private static void playPhone(User u) {
+        System.out.println(u.getName() + "去玩耍手机");
+    }
+
+    private static void playComputer(User u) {
+        System.out.println(u.getName() + "去玩耍电脑");
     }
 }
